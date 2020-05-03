510918a340ba62d5612750a189289fa983fa22e8
rnag <rnag@umich.edu>
2020-05-02 20:31:59 -0400

    work

--- get_friends.py
+++ get_friends.py
@@ -28,12 +28,45 @@ def get_real_username(user_name):
     return user_name
 
 def analyze_user(user_name):
+
+    #data structures needed:
+    # set containing usernames of friends (definitely needed)
+    # dictionary mapping username to name  (probably needed)
+    # dictionary mapping username to set of mutual friends
+
+
+
     #verify user exists
     user_name = get_real_username(user_name)
-    friends = Twitter.friends(user_name)
-    for friend in friends:
-        print(friend.name)
+    friends_info = Twitter.friends(user_name)
+    
+    #dictionary for mapping username to name
+    friends_username = dict()
+
+    #first get set of all friends I have
+    friends = set() #all of my friends (contains username, not actual name to avoid duplicate names)
+    
+    #assign set of friends and also dictionary mapping username to name
+    for friend in friends_info:
+        if friend.screen_name not in friends:
+            friends.add(friend.screen_name)
+            friends_username[friend.screen_name] = friend.name
+
+
+    friend_connections = dict() #dictionary that stores username and all mutual friends
+    import time
+    for friend in friends: #for each friend
+        mutual_friends = set()
+        time.sleep(5)
+        friend_info = Twitter.friends(friend)
+        for info in friend_info:
+            if info.screen_name in friends: #if we have a mutual friend
+                mutual_friends.add(info.screen_name)
+        friend_connections[friend] = mutual_friends
+
         
+        
+
     breakpoint()
     x = 4
 
@@ -49,7 +82,7 @@ access_token_secret = 'yH9eeZtLkYjxcam2PGjpaQWw3g5sdSGjakarcIsto7nED'
 
 auth.set_access_token(access_token, access_token_secret)
 
-Twitter = tweepy.API(auth)
+Twitter = tweepy.API(auth,wait_on_rate_limit=True)
 
 
 while True:
