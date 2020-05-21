775ac52ce274c3059f8d154da0b4451d325f228a
rnag <rnag@umich.edu>
2020-05-21 18:20:23 -0400

    final commit

--- get_friends.py
+++ get_friends.py
@@ -125,12 +125,12 @@ def analyze_user(user_name):
 
 import tweepy
 
-consumer_key= 'FvflCo1aIlcLbeFfq9unJ9rli'#removed for privacy
-consumer_secret = 'MRMYvUFeTgVGCwktHfPvPt3EAseDIfMZfzCwrZ0tIvagC6hWat'
+consumer_key= ''#removed for privacy
+consumer_secret = ''
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-access_token = '1244794411017535489-C2RFnu2E0yiJdEYVDlaEJBjR7mAkJm'
-access_token_secret = 'yH9eeZtLkYjxcam2PGjpaQWw3g5sdSGjakarcIsto7nED'
+access_token = ''
+access_token_secret = ''
 
 auth.set_access_token(access_token, access_token_secret)
 
